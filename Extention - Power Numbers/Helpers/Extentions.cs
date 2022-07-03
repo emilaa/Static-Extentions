da8678ef421025f9c470f8ccb4c0bb8627b9d3bb
emilaa <emilaa@code.edu.az>
2022-07-03 22:06:12 +0400

    New add 'cs' file

---
+++ Extention - Power Numbers/Helpers/Extentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extention___Power_Numbers.Helpers
+{
+    public static class Extentions
+    {
+        public static double PowerNumbers(this int num1, int num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+    }
+}
---
+++ Extention - Power Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using Extention___Power_Numbers.Helpers;
+using System;
+
+namespace Extention___Power_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            var result = num1.PowerNumbers(3);
+
+            Console.WriteLine(result);
+        }
+    }
+}
